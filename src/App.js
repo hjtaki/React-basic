114ecd50e2ae1a1dc797f30281b361e199a089b6
hjtaki <hjtaki@gmail.com>
2020-01-29 21:19:09 -0800

    event

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
     //initialize
     this.state = {
       mode: 'read',
-      Subject: { title: 'WEB', subtitle: 'WWW' },
+      selected_contect_id: 2,
+      Subject: { title: 'WEB ', subtitle: 'WWW' },
       welcome: { title: 'welcome', desc: 'Hello, React' },
       contents: [
         { id: 1, title: 'HTML', desc: 'HTML is Hypertext' },
@@ -27,34 +28,42 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      // _title = this.state.contents[0].title;
+      // _desc = this.state.contents[0].desc;
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_contect_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
+
     console.log('render is', this)
     return (
-      <div className="App">
-        {/* <Subject
+      <div className="App" >
+        <Subject
           title={this.state.Subject.title}
-          subtitle={this.state.Subject.subtitle}>
-        </Subject> */}
-        <header>
-          <h1><a href='/' onClick={function (e) {
-            console.log('event in', this);
-            e.preventDefault();
-            return;
-            // to provent event's default function
-            e.preventDefault();
-            this.setState({
-              mode: 'welcome'
-            })
-            // when it cannot find this
-          }.bind(this)}>{this.state.Subject.title}</a></h1>
-          {this.state.Subject.subtitle}
-        </header>
+          subtitle={this.state.Subject.subtitle}
+          onChangePage={function () {
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}
+        >
+        </Subject>
 
-        <TOC data={this.state.contents}></TOC>
+        <TOC
+          onChangePage={function () {
+            this.setState({
+              mode: 'read',
+              selected_contect_id: 0
+            })
+          }.bind(this)}
+          data={this.state.content}></TOC>
         <Content title={_title} subtitle={_desc}></Content>
-      </div>
+      </div >
     );
   }
 }
